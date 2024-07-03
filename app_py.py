293ef9764bf0c8254e3e5dacca3abdcf62cac559
enterumez <131345035+enterumez@users.noreply.github.com>
2024-07-04 03:34:19 +0900

    app_py.py

--- app_py.py
+++ app_py.py
@@ -14,4 +14,14 @@ if submit_btn:
 elif cancel_btn:
     st.write('キャンセルしました')
 
-<iframe class="speakerdeck-iframe" frameborder="0" src="https://speakerdeck.com/player/8eaed11763ea4d0d901a4bf870ac52c7" title="プライシングについて" allowfullscreen="true" style="border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: auto; aspect-ratio: 560 / 315;" data-ratio="1.7777777777777777"></iframe>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Speaker Deck Embed</title>
+</head>
+<body>
+    <iframe class="speakerdeck-iframe" frameborder="0" src="https://speakerdeck.com/player/8eaed11763ea4d0d901a4bf870ac52c7" title="プライシングについて" allowfullscreen="true" style="border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: auto; aspect-ratio: 560 / 315;" data-ratio="1.7777777777777777"></iframe>
+</body>
+</html>
